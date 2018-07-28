328c33c91916265ca77f654ca815a476ddfa91af
Karl Niebuhr <karl.booklover@gmail.com>
2018-07-28 07:16:40 -0400

    del counter

--- scrap.py
+++ scrap.py
@@ -7,7 +7,6 @@ set_url = 'https://servicios.set.gov.py/eset-publico/ciudadano/recuperar'
 
 # loop through all URLs
 id_range = 10000000
-id = 1
 
 with open ('datos.csv','w',newline='') as csvfile:
    writer=csv.writer(csvfile)
@@ -16,7 +15,6 @@ with open ('datos.csv','w',newline='') as csvfile:
        payload = {"cedula": id}
        result = requests.get(set_url, params=payload)
        res_json = result.json()
-       id+=1
        if(res_json["presente"] == False):
             print("No existe el nro de cedula")
        else:
