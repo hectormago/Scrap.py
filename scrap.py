52770b740c550e83f39d3eb578a5b48b26d722b2
karl <karlbooklover@gmail.com>
2018-07-29 21:26:53 -0400

    docs

--- scrap.py
+++ scrap.py
@@ -1,23 +1,23 @@
 # Made with love by Karlpy
-import grequests
-import csv
-import json
+import grequests, csv, json
 
 set_url = 'https://servicios.set.gov.py/eset-publico/ciudadano/recuperar?cedula='
 
 # Cedula (id) range
-id_range = 100
+id_range = 10
+urls = [set_url + str(ced) for ced in range(id_range)]
 
 with open ('datos.csv','w',newline='') as csvfile:
     writer=csv.writer(csvfile)
     writer.writerow(['cedula', 'nombres', 'apellidoPaterno', 'apellidoMaterno', 'nombreCompleto'])
-    urls = [set_url + str(ced) for ced in range(id_range)]
-    requests_unsent = (grequests.get(u) for u in urls)
-    requests_iterable = grequests.imap(requests_unsent, size=10)
 
-    # do it asynchronously
+    # grequests automatically creates a session to avoid TCP overhead. Let's create the requests generator
+    requests_unsent = (grequests.get(u) for u in urls)
+    # imap concurrently converts a generator object of Requests to a generator of Responses.
+    requests_iterable = grequests.imap(requests_unsent, size=10)
+    # iterate over the responses generator
     for response in requests_iterable:
-        response_json = response.json()
+        response_json = json.loads(response.text)
         if(response_json["presente"] == False):
             print("No existe el nro de cedula")
         else:
